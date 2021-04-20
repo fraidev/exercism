8432932e366acc50767f62d4b0d5a8259fb8a7c5
Felipe Cardozo <fraifelipe@gmail.com>
2021-04-20 00:43:50 -0300

    init go exercism

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,31 +1,14 @@
 pub fn reply(message: &str) -> &str {
-    let message_without_whitespaces: String = message.split_whitespace().collect();
-
-    let all_alphabetic_chars = message_without_whitespaces
-        .chars()
-        .filter(|x| x.is_alphabetic())
-        .collect::<Vec<char>>();
-
-    if !all_alphabetic_chars.is_empty()
-        && all_alphabetic_chars.iter().all(|x| x.is_uppercase())
-        && message_without_whitespaces.chars().last().unwrap() == '?'
-    {
-        return "Calm down, I know what I'm doing!";
+    match message.trim() {
+        m if m.is_empty() => "Fine. Be that way!",
+        m if m.ends_with("?") && is_yelling(message.trim()) => "Calm down, I know what I'm doing!",
+        m if m.ends_with("?") => "Sure.",
+        m if is_yelling(m) => "Whoa, chill out!",
+        _ => "Whatever.",
     }
+}
 
-    if !message_without_whitespaces.is_empty()
-        && message_without_whitespaces.chars().last().unwrap() == '?'
-    {
-        return "Sure.";
-    }
-
-    if !all_alphabetic_chars.is_empty() && all_alphabetic_chars.iter().all(|x| x.is_uppercase()) {
-        return "Whoa, chill out!";
-    }
-
-    if message_without_whitespaces.is_empty() {
-        return "Fine. Be that way!";
-    }
-
-    "Whatever."
+fn is_yelling(message: &str) -> bool {
+    let have_letters: bool = message.chars().filter(|x| x.is_alphabetic()).count() > 0;
+    message.to_uppercase() == message && have_letters
 }
